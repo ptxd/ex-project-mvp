37add60c00f0609e05f3594558b5202fbce54b18
ptxd <ptxdas@gmail.com>
2017-06-30 21:57:20 -0600

    phase 4 - mid

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,12 +1,23 @@
-import React from 'react';
 
-const HomePage = () =>
-   <div className="main-image">
-        <div className="mainbody-div">
-           <button className="banner-btn">
-                 <span>Learn More</span>
-            </button>
-        </div> 
-   </div>;
+import React, { Component } from 'react';
+import video from '../video/background.mp4'
 
-export default HomePage
+class HomePage extends Component {
+
+    render() {
+        return (    
+            <div>
+                <video className='main-image' autoPlay loop muted>
+                    <source src={video} type='video/mp4'/>
+                </video>
+                <div className="mainbody-div">
+                    <button className="banner-btn">
+                        <span>Learn More</span>
+                    </button>
+                </div>   
+            </div>
+        );
+    }
+}
+
+export default HomePage;
