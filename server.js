7cf075cda7d1b2f750c1238c4f3590caff9e3c88
ptxd <ptxdas@gmail.com>
2017-07-03 14:40:58 -0600

    3.1

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ const app = express();
 //this is the server's port number
 const port = process.env.PORT || 5000;
 
-
 // Parse incoming requests data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
