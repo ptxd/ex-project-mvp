79ae807916e2f25194a6fa30ee2aaf9324296edd
ptxd <ptxdas@gmail.com>
2017-07-09 22:04:38 -0600

    refactor2

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import ProductContainer from '../containers/ProductList/ProductContainer';
+import SideBar from '../containers/SideBar/SideBarView';
 
 class ProductDetail extends Component {
     render() {
         return (
             <div className="product-main">
+                <SideBar/>
                 <div className="product-container">
                     <ProductContainer></ProductContainer>
                 </div>    
---
+++ src/containers/SideBar/SideBarView.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SideBar extends Component {
+    render() {
+        return (
+            <div className="sidebar-wrapper">
+                
+            </div>
+        );
+    }
+}
+
+export default SideBar;
