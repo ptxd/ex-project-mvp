6acff6f2077b4b9f819f0ecb5e695083920042fc
ptxd <ptxdas@gmail.com>
2017-07-09 22:59:18 -0600

    refactor4

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -3,33 +3,42 @@ import MultipleAreaCharts from '../containers/DashChart/DashChart';
 import SearchBarContainer from '../containers/SearchBar/SearchBarContainer';
 import ProductTrackerContainer from '../containers/ProductTracker/ProductTrackerContainer';
 import TableList from './table';
+import SideBar from '../containers/SideBar/SideBarView';
 
 class Dashboard extends Component {
   render() {
     return (
       <div className="dashboard">
-        <div className="area-chart">
-          <div className="recharts-left">
-            <MultipleAreaCharts />
-          </div>
-           <span>
-          <div className="recharts-right">    
-             <MultipleAreaCharts />
-          </div>  
-          </span>           
-        </div> 
-        <div className="dash-trend-container">
-          <TableList></TableList>
+        <div className="dashboard-side-bar">
+          <SideBar></SideBar>
         </div>
-        <div className="dash-search">       
-            <SearchBarContainer />
+        <div className="dashboard-container">
+          <div className="area-chart">
+              <div className="recharts-left">
+                <MultipleAreaCharts />
+              </div>
+              <span>
+              <div className="recharts-right">    
+                <MultipleAreaCharts />
+              </div>  
+              </span>           
+          </div> 
+
         </div>
-        <div className="dash-table">
-            <ProductTrackerContainer />
-        </div>    
       </div>
     );
   }
 }
 
 export default Dashboard;
+
+
+  //  <div className="dash-trend-container">
+  //         <TableList></TableList>
+  //         </div>
+          //   <div className="dash-search">       
+          //   <SearchBarContainer />
+          // </div>
+          // <div className="dash-table">
+          //   <ProductTrackerContainer />
+          // </div> 
