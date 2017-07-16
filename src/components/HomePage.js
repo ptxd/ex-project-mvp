6621a5395fceb21d7a0742802b4c3d9cb15db833
ptxd <ptxdas@gmail.com>
2017-07-15 21:03:29 -0600

    front page design

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,16 +7,23 @@ class HomePage extends Component {
     render() {
         return (    
             <div>
-                <div className="mainbody-div">   
-                        <button className="banner-btn">
-                            <Link to='/about'>
-                                <span>Learn More</span>
-                            </Link>
-                        </button>
-                    </div>
+                <div className="mainbody-div"> 
+                    <div className="exstral">
+                        <p className=" tracking-in-expand">EXSTRAL</p> 
+                    </div> 
+                    <button className="banner-btn">
+                        <Link to='/about'>
+                            <span>Learn More</span>
+                        </Link>
+                    </button>
+                </div>
                 <div className="about-us">
-                    <div><p className="about-text">How It Works</p></div>
+                    <div><p className="about-text">WHAT WE DO</p></div>
                     <About/>
+                </div>
+                <div className="hiw-div">
+                    <div><p className="hiw-text">HOW IT WORKS</p></div>
+                    <div></div>
                 </div>   
             </div>
         );
